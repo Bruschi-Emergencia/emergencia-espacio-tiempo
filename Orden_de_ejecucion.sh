f4c6d1cb16591225c7fe2f4200a8e6fad3bf0a7c
Bruschi-Emergencia <info@halfing.com.ar>
2026-04-07 20:16:37 -0300

    Add files via upload

---
+++ Orden_de_ejecucion.sh
@@ -0,0 +1,6 @@
+python red_3D.py
+python expansion.py
+python mcmc_supernovas.py   # (requiere datos reales o los simula)
+python nucleos_incompresibles.py
+python bariogenesis.py
+python defecto_topologico.py
